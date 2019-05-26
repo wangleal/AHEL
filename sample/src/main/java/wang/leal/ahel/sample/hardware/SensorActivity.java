3c46c1085763776486e38cf5907648ea1dd816e1
wangli <wangli@3commas.cn>
2019-05-26 16:34:09 +0800

    add sensor orientation

--- sample/src/main/java/wang/leal/ahel/sample/hardware/SensorActivity.java
+++ sample/src/main/java/wang/leal/ahel/sample/hardware/SensorActivity.java
@@ -1,28 +1,34 @@
 package wang.leal.ahel.sample.hardware;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import wang.leal.ahel.R;
-import wang.leal.ahel.hardware.sensor.AccelerometerSensor;
+import wang.leal.ahel.service.sensor.OrientationDetection;
 
 public class SensorActivity extends AppCompatActivity {
-    private AccelerometerSensor sensor;
+    private OrientationDetection orientationDetection;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
         TextView tvInfo = findViewById(R.id.tv_info);
-        sensor = new AccelerometerSensor(getApplicationContext());
-        sensor.start();
+        orientationDetection = new OrientationDetection(getApplicationContext()) {
+            @Override
+            public void onOrientationChanged(int orientation) {
+                tvInfo.setText("orientation:"+orientation );
+            }
+        };
+        orientationDetection.enable();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        sensor.release();
+        orientationDetection.disable();
     }
 }
--- sample/src/test/java/wang/leal/ahel/ExampleUnitTest.java
+++ sample/src/test/java/wang/leal/ahel/ExampleUnitTest.java
@@ -12,7 +12,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-//        assertEquals(4, 2 + 2);
-        System.out.println(Math.atan2(-1,1)*57.29577957855f);
+        assertEquals(4, 2 + 2);
     }
 }
