70c268b07b7f1a72819f5d64a9c68140007bbcd9
wangli <wangli@3commas.cn>
2019-10-16 15:37:14 +0800

    add color util

---
+++ utils/src/main/java/wang/leal/ahel/utils/ColorUtil.java
@@ -0,0 +1,36 @@
+package wang.leal.ahel.utils;
+
+import android.graphics.Color;
+
+public class ColorUtil {
+
+    public static int getCurrentColor(float fraction, int startColor, int endColor) {
+        int redCurrent;
+        int blueCurrent;
+        int greenCurrent;
+        int alphaCurrent;
+
+        int redStart = Color.red(startColor);
+        int blueStart = Color.blue(startColor);
+        int greenStart = Color.green(startColor);
+        int alphaStart = Color.alpha(startColor);
+
+        int redEnd = Color.red(endColor);
+        int blueEnd = Color.blue(endColor);
+        int greenEnd = Color.green(endColor);
+        int alphaEnd = Color.alpha(endColor);
+
+        int redDifference = redEnd - redStart;
+        int blueDifference = blueEnd - blueStart;
+        int greenDifference = greenEnd - greenStart;
+        int alphaDifference = alphaEnd - alphaStart;
+
+        redCurrent = (int) (redStart + fraction * redDifference);
+        blueCurrent = (int) (blueStart + fraction * blueDifference);
+        greenCurrent = (int) (greenStart + fraction * greenDifference);
+        alphaCurrent = (int) (alphaStart + fraction * alphaDifference);
+
+        return Color.argb(alphaCurrent, redCurrent, greenCurrent, blueCurrent);
+    }
+
+}
