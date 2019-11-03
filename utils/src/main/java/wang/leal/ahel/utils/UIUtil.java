343db93478516ef5cca36b874a0958c7f2ad98c8
wangli <wangli@3commas.cn>
2019-11-03 21:21:59 +0800

    add UI util methods

--- utils/src/main/java/wang/leal/ahel/utils/UIUtil.java
+++ utils/src/main/java/wang/leal/ahel/utils/UIUtil.java
@@ -1,9 +1,14 @@
 package wang.leal.ahel.utils;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Build;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import android.view.WindowManager;
 
 public class UIUtil {
 
@@ -30,5 +35,50 @@ public class UIUtil {
     public static int px2dip(Context context,int px){
         float scale = context.getResources().getDisplayMetrics().density;
         return (int)(px / scale +0.5f);
+    }
+
+    public static boolean isShowNavBar(Context context) {
+        if (null == context) {
+            return false;
+        }
+        Rect outRect = new Rect();
+        try {
+            ((Activity) context).getWindow().getDecorView().getWindowVisibleDisplayFrame(outRect);
+        } catch (ClassCastException e) {
+            e.printStackTrace();
+            return false;
+        }
+        int activityHeight = outRect.height();
+        int statusBarHeight = getStatusBarHeight(context);
+        int remainHeight = getRealHeight((Activity) context) - statusBarHeight;
+        return activityHeight != remainHeight;
+
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+
+    }
+
+    public static int getRealHeight(Activity activity) {
+        WindowManager windowManager = activity.getWindowManager();
+        Point point = new Point();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            windowManager.getDefaultDisplay().getRealSize(point);
+        } else {
+            windowManager.getDefaultDisplay().getSize(point);
+        }
+        return point.y;
+    }
+
+    public static int getNavigationBarHeight(Context context){
+        Resources resources = context.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height","dimen", "android");
+        return resources.getDimensionPixelSize(resourceId);
     }
 }
