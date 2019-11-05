e261750d54f3dff85db85ff1ded8bafe8646a671
wangli <wangli@3commas.cn>
2019-11-05 11:35:18 +0800

    添加中英文判断

--- utils/src/main/java/wang/leal/ahel/utils/StringUtil.java
+++ utils/src/main/java/wang/leal/ahel/utils/StringUtil.java
@@ -13,4 +13,55 @@ public class StringUtil {
             return message.getBytes(Charset.forName("UTF-8")).length;
         }
     }
+
+    /**
+     * 获取字符长度，中文算一个，英文算两个
+     * @param message   字符
+     * @return 长度
+     */
+    public static int getStringLengthByC1E2(String message){
+        int valueLength = 0;
+        String chinese = "[\u0391-\uFFE5]";
+        for (int i = 0; i < message.length(); i++) {
+            String temp = message.substring(i, i + 1);
+            if (temp.matches(chinese)) {
+                valueLength += 2;
+            } else {
+                valueLength += 1;
+            }
+        }
+        return valueLength;
+    }
+
+    public static boolean hasChinese(String message){
+        String chinese = "[\u0391-\uFFE5]";
+        for (int i = 0; i < message.length(); i++) {
+            String temp = message.substring(i, i + 1);
+            if (temp.matches(chinese)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasEnglish(String message){
+        return message.matches(".*[a-zA-z].*");
+    }
+
+    public static boolean isAllNumber(String message){
+        return message.matches("[0-9]+");
+    }
+
+    public static boolean isAllEnglish(String message){
+        return message.matches("[a-zA-Z]+");
+    }
+
+    public static boolean isAllChinese(String message){
+        return message.matches("[\u4e00-\u9fa5]+");
+    }
+
+    public static boolean hasNumber(String message){
+        return message.matches(".*[0-9].*");
+    }
+
 }
