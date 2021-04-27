326b4022a5ecf9b7fbe86aca639d7e5dccf9ef97
wangli <wwangliw@qq.com>
2021-04-27 11:24:39 +0800

    支持java静态调用

--- bus/src/main/java/wang/leal/ahel/bus/Bus.kt
+++ bus/src/main/java/wang/leal/ahel/bus/Bus.kt
@@ -2,18 +2,22 @@ package wang.leal.ahel.bus
 
 object Bus {
 
+    @JvmStatic
     fun listen(vararg actions:String):ListenService{
         return BusService.listenService(*actions)
     }
 
+    @JvmStatic
     fun post(action:String):PostService{
         return BusService.postService(action)
     }
 
+    @JvmStatic
     fun request(action:String):RequestService{
         return BusService.requestService(action)
     }
 
+    @JvmStatic
     fun provide(action: String):ProviderService{
         return BusService.providerService(action)
     }
